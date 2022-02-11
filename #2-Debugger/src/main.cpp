5f0bb0c8c1e0b1988888e5c8f3b386e5823b98f4
Clément <clementbourhis29@gmail.com>
2022-02-11 13:07:28 +0100

    Push #2 to #4

---
+++ #2-Debugger/src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+  int n = 3;
+  std::cout << "boup" << n << std::endl;
+  std::cout << "pip poup" << std::endl;
+}
--- Simple-Cpp-Setup-main/src/main.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <iostream>
-
-void boup() {
-    std::cout << "pop" << std::endl;
-}
